e4f5be6b92574f5fa2a94004bfae162c77bba9d3
fazanki <fazanki@gmail.com>
2020-01-07 10:57:14 +0000

    adding modal working version

--- api/user/index.ts
+++ api/user/index.ts
@@ -1,8 +1,6 @@
 import * as express from 'express'
 import * as log4jui from '../lib/log4jui'
 const logger = log4jui.getLogger('auth')
-import { UserProfileModel } from './user'
-import * as jwtDecode from 'jwt-decode'
 
 export const router = express.Router({ mergeParams: true })
 
@@ -18,19 +16,14 @@ function handleUserRoute(req, res) {
   };
   const userRoles = 'caseworker'
 
-  function getUserTimeouts() {
+  function getUserIdleTimeouts() {
     if (userRoles.indexOf('caseworker') !== -1) {
       return timeOuts['caseworker']
     }
   }
-
-  // const sessionTimeOut = getUserTimeouts()
-  // const sessionTimeStamp = Math.floor(jwtDecode(req.session.auth.token).exp * 1000)
-  const now = new Date()
-  /* Session timeout */
-  // const sessionTimeStamp = now.setMinutes(now.getMinutes() + 1)
   const UserDetails = {
     ...req.session.user,
+    idleTime: getUserIdleTimeouts()
   }
 
   try {
