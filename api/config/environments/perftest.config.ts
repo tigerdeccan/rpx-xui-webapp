c0f1f121c307a05e7110ec44d93a3a6cd9fea42d
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2020-03-23 14:24:40 +0000

    Update perftest.config.ts

--- api/config/environments/perftest.config.ts
+++ api/config/environments/perftest.config.ts
@@ -15,7 +15,7 @@ export default {
             api: 'http://dm-store-perftest.service.core-compute-perftest.internal',
         },
         idam: {
-            idamApiUrl: 'http://idam-api-perftest.service.core-compute-perftest.internal',
+            idamApiUrl: 'https://idam-api.perftest.platform.hmcts.net',
             idamClientID: 'xuiwebapp',
             idamLoginUrl: 'https://idam-web-public.perftest.platform.hmcts.net',
             indexUrl: '/',
